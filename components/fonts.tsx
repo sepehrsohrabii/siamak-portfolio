357c3c40cf88beaf115c6060c6971c5843e69b73
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2023-10-23 00:23:26 +0330

    work on home page

---
+++ components/fonts.tsx
@@ -0,0 +1,9 @@
+import { Inter, Roboto_Condensed } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+const roboto_c = Roboto_Condensed({
+  weight: ["300", "400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
---
+++ components/header.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useState } from "react";
+import Sidebar from "./sidebar";
+
+export default function Header() {
+  const [open, setOpen] = useState(true);
+  return (
+    <header className="w-full py-10 absolute ">
+      <div className="mx-40 flex justify-between">
+        <h1 className="text-4xl font-bold">Siamak</h1>
+        <button onClick={() => setOpen(!open)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <Sidebar open={open} setOpen={setOpen} />
+      </div>
+    </header>
+  );
+}
