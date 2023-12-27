9c9f7cdc8d2f034d1c5f3e9089d5bc76254705e5
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2023-12-27 19:59:18 +0330

    Add local font support for Roboto Condensed

--- app/(publicSide)/contact/page.tsx
+++ app/(publicSide)/contact/page.tsx
@@ -10,138 +10,143 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const ContactPage: React.FC = () => {
+const ContactPage = () => {
    return (
       <>
-         <motion.div className='ms-5 items-center pt-32 md:ms-40 md:flex'>
-            <motion.div
-               initial={{ x: -200 }}
-               animate={{ x: 0 }}
-               transition={{ duration: 1 }}
-               className='basis-1/2'
-            >
-               <Heading1 className='text-gray-800'>Get in touch</Heading1>
-               <motion.div
-                  initial={{ x: -200 }}
-                  animate={{ x: 0 }}
-                  transition={{ duration: 1 }}
-                  className=''
-               >
-                  <Heading4 className='text-gray-700'>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </Heading4>
+         {typeof window !== 'undefined' && (
+            <>
+               <motion.div className='ms-5 items-center pt-32 md:ms-40 md:flex'>
+                  <motion.div
+                     initial={{ x: -200 }}
+                     animate={{ x: 0 }}
+                     transition={{ duration: 1 }}
+                     className='basis-1/2'
+                  >
+                     <Heading1 className='text-gray-800'>Get in touch</Heading1>
+                     <motion.div
+                        initial={{ x: -200 }}
+                        animate={{ x: 0 }}
+                        transition={{ duration: 1 }}
+                        className=''
+                     >
+                        <Heading4 className='text-gray-700'>
+                           Lorem ipsum dolor sit amet consectetur adipisicing
+                           elit.
+                        </Heading4>
+                     </motion.div>
+                     <div className='mt-7 md:mt-16'>
+                        <div className='md:flex md:justify-between md:pe-36'>
+                           <div className='mt-10 md:mt-0'>
+                              <Link
+                                 href='#'
+                                 className='items-center md:flex md:flex-col'
+                              >
+                                 <Image
+                                    className='w-10 md:w-14'
+                                    src='/icons/phone-call.png'
+                                    alt='Phone Icon'
+                                    width={64}
+                                    height={64}
+                                 />
+                                 <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
+                                    +989113332222
+                                 </Paragraph1>
+                              </Link>
+                           </div>
+                           <div className='mt-10 md:mt-0'>
+                              <Link
+                                 href='#'
+                                 className='items-center md:flex md:flex-col'
+                              >
+                                 <Image
+                                    className='w-10 md:w-14'
+                                    src='/icons/gmail.png'
+                                    alt='Phone Icon'
+                                    width={64}
+                                    height={64}
+                                 />
+                                 <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
+                                    graph@gmail.com
+                                 </Paragraph1>
+                              </Link>
+                           </div>
+                           <div className='mt-10 md:mt-0'>
+                              <Link
+                                 href='#'
+                                 className='items-center md:flex md:flex-col'
+                              >
+                                 <Image
+                                    className='w-10 md:w-14'
+                                    src='/icons/address.png'
+                                    alt='Phone Icon'
+                                    width={64}
+                                    height={64}
+                                 />
+                                 <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
+                                    Rasht, Iran
+                                 </Paragraph1>
+                              </Link>
+                           </div>
+                        </div>
+                     </div>
+                  </motion.div>
+                  <motion.div
+                     initial={{ x: +200 }}
+                     animate={{ x: 0 }}
+                     transition={{ duration: 1 }}
+                     className='mt-10 basis-1/2 md:mt-0'
+                  >
+                     <LocationMap />
+                  </motion.div>
                </motion.div>
-               <div className='mt-7 md:mt-16'>
-                  <div className='md:flex md:justify-between md:pe-36'>
-                     <div className='mt-10 md:mt-0'>
-                        <Link
-                           href='#'
-                           className='items-center md:flex md:flex-col'
-                        >
+               <motion.div className='my-20 items-center bg-stone-300 px-5 py-20 md:px-40'>
+                  <div className='justify-between md:flex'>
+                     <Heading3 className='self-center text-center text-gray-700 md:text-left'>
+                        Social Media
+                     </Heading3>
+                     <div className='mt-5 md:mt-0'>
+                        <Link href='#' className='flex flex-col items-center'>
                            <Image
-                              className='w-10 md:w-14'
-                              src='/icons/phone-call.png'
+                              src='/icons/instagram.png'
                               alt='Phone Icon'
-                              width={64}
-                              height={64}
+                              width={40}
+                              height={40}
                            />
                            <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
-                              +989113332222
+                              @graph_arch_studio
                            </Paragraph1>
                         </Link>
                      </div>
-                     <div className='mt-10 md:mt-0'>
-                        <Link
-                           href='#'
-                           className='items-center md:flex md:flex-col'
-                        >
+                     <div className='mt-5 md:mt-0'>
+                        <Link href='#' className='flex flex-col items-center'>
                            <Image
-                              className='w-10 md:w-14'
-                              src='/icons/gmail.png'
+                              src='/icons/telegram.png'
                               alt='Phone Icon'
-                              width={64}
-                              height={64}
+                              width={40}
+                              height={40}
                            />
                            <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
-                              graph@gmail.com
+                              +989112345678
                            </Paragraph1>
                         </Link>
                      </div>
-                     <div className='mt-10 md:mt-0'>
-                        <Link
-                           href='#'
-                           className='items-center md:flex md:flex-col'
-                        >
+                     <div className='mt-5 md:mt-0'>
+                        <Link href='#' className='flex flex-col items-center'>
                            <Image
-                              className='w-10 md:w-14'
-                              src='/icons/address.png'
+                              src='/icons/whatsapp.png'
                               alt='Phone Icon'
-                              width={64}
-                              height={64}
+                              width={40}
+                              height={40}
                            />
                            <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
-                              Rasht, Iran
+                              +989112345678
                            </Paragraph1>
                         </Link>
                      </div>
                   </div>
-               </div>
-            </motion.div>
-            <motion.div
-               initial={{ x: +200 }}
-               animate={{ x: 0 }}
-               transition={{ duration: 1 }}
-               className='mt-10 basis-1/2 md:mt-0'
-            >
-               <LocationMap />
-            </motion.div>
-         </motion.div>
-         <motion.div className='my-20 items-center bg-stone-300 px-5 py-20 md:px-40'>
-            <div className='justify-between md:flex'>
-               <Heading3 className='self-center text-center text-gray-700 md:text-left'>
-                  Social Media
-               </Heading3>
-               <div className='mt-5 md:mt-0'>
-                  <Link href='#' className='flex flex-col items-center'>
-                     <Image
-                        src='/icons/instagram.png'
-                        alt='Phone Icon'
-                        width={40}
-                        height={40}
-                     />
-                     <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
-                        @graph_arch_studio
-                     </Paragraph1>
-                  </Link>
-               </div>
-               <div className='mt-5 md:mt-0'>
-                  <Link href='#' className='flex flex-col items-center'>
-                     <Image
-                        src='/icons/telegram.png'
-                        alt='Phone Icon'
-                        width={40}
-                        height={40}
-                     />
-                     <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
-                        +989112345678
-                     </Paragraph1>
-                  </Link>
-               </div>
-               <div className='mt-5 md:mt-0'>
-                  <Link href='#' className='flex flex-col items-center'>
-                     <Image
-                        src='/icons/whatsapp.png'
-                        alt='Phone Icon'
-                        width={40}
-                        height={40}
-                     />
-                     <Paragraph1 className='mt-2 text-gray-700 md:mt-5'>
-                        +989112345678
-                     </Paragraph1>
-                  </Link>
-               </div>
-            </div>
-         </motion.div>
+               </motion.div>
+            </>
+         )}
       </>
    );
 };
--- components/fonts.tsx
+++ components/fonts.tsx
@@ -1,4 +1,5 @@
 import { Inter, Roboto_Condensed } from 'next/font/google';
+import localFont from 'next/font/local';
 
 export const inter = Inter({ subsets: ['latin'] });
 
@@ -7,4 +8,54 @@ export const roboto_c = Roboto_Condensed({
    style: ['normal'],
    subsets: ['latin'],
    display: 'swap',
+});
+
+export const roboto_c_local = localFont({
+   src: [
+      {
+         path: '../public/fonts/RobotoCondensed-Black.ttf',
+         weight: '900',
+         style: 'normal',
+      },
+      {
+         path: '../public/fonts/RobotoCondensed-ExtraBold.ttf',
+         weight: '800',
+         style: 'normal',
+      },
+      {
+         path: '../public/fonts/RobotoCondensed-Bold.ttf',
+         weight: '700',
+         style: 'normal',
+      },
+      {
+         path: '../public/fonts/RobotoCondensed-SemiBold.ttf',
+         weight: '600',
+         style: 'normal',
+      },
+      {
+         path: '../public/fonts/RobotoCondensed-Medium.ttf',
+         weight: '500',
+         style: 'normal',
+      },
+      {
+         path: '../public/fonts/RobotoCondensed-Regular.ttf',
+         weight: '400',
+         style: 'normal',
+      },
+      {
+         path: '../public/fonts/RobotoCondensed-Light.ttf',
+         weight: '300',
+         style: 'normal',
+      },
+      {
+         path: '../public/fonts/RobotoCondensed-ExtraLight.ttf',
+         weight: '200',
+         style: 'normal',
+      },
+      {
+         path: '../public/fonts/RobotoCondensed-Thin.ttf',
+         weight: '100',
+         style: 'normal',
+      },
+   ],
 });
