3f4293bfc9d1c97039d86b9d3ade6776da840024
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-01-21 20:11:04 +0330

    Update next.config.js with experimental server actions and server components external packages

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
-module.exports = nextConfig;
+// const nextConfig = {};
+// module.exports = nextConfig;
 
-// const withImages = require('next-images');
-// module.exports = withImages({});
+// // const withImages = require('next-images');
+// // module.exports = withImages({});
 
-// const nextConfig = {
-//     reactStrictMode: true,
-//     compiler: {
-//        styledComponents: true,
-//     },
-//     experimental: {
-//        serverActions: true,
-//        serverComponentsExternalPackages: ['mongoose'],
-//     },
-//  };
-//  module.exports = nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    compiler: {
+       styledComponents: true,
+    },
+    experimental: {
+       serverActions: true,
+       serverComponentsExternalPackages: ['mongoose'],
+    },
+ };
+ module.exports = nextConfig;
  
-//  const withImages = require('next-images');
-//  module.exports = withImages({});
+ const withImages = require('next-images');
+ module.exports = withImages({});
  
